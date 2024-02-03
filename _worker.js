cdd8e5747c9182eae01da6785fb6c5afb6eb01d4
mahmooddavid256 <158694464+mahmooddavid256@users.noreply.github.com>
2024-02-03 09:55:00 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+let userID = 'tplslbcy35xzu78aqplui256.pages.dev/82a9fb41-fea6-4ec6-979a-2f919187190a ';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
